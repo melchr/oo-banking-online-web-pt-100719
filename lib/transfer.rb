a38bc54b30687235dd6e5542e3429b5bdb12d933
Melissa Christman <melchrstmn@gmail.com>
2019-11-08 01:50:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,11 @@ class Transfer
   end
   
   def reverse_transfer
-    
+    if self.execute_transaction
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = 'reversed'
+    end
   end
   
 end
